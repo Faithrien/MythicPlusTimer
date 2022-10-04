d43e810e417a084ea5535ce23228174e13f8c0f4
yankikucuk <kendinikertenkelebek@me.com>
2022-10-04 15:48:23 +0300

    Init

---
+++ MythicPlusTimer.lua
@@ -0,0 +1,70 @@
+MythicPlusTimerDB = {
+  ["config"] = {
+    ["color_affixes"] = "FFFFFFFF",
+    ["color_objective_completed_time"] = "FF808080",
+    ["show_deathcounter"] = true,
+    ["color_chest_time"] = "FFFFFFFF",
+    ["progress_tooltip"] = true,
+    ["align_right"] = false,
+    ["show_affixes_as_icons"] = false,
+    ["objective_time_inchat"] = true,
+    ["color_deathcounter_timelost"] = "FFFF0000",
+    ["show_absolute_numbers"] = false,
+    ["show_reapingtimer"] = true,
+    ["position"] = {
+      ["top"] = -242.2777709960938,
+      ["left"] = -124.9997634887695,
+      ["relative_point"] = "TOPRIGHT",
+    },
+    ["show_enemy_forces_bar"] = false,
+    ["objective_time_perlevel"] = false,
+    ["objective_time_perlevelaffix"] = true,
+    ["show_pridefultimer"] = true,
+    ["objective_time_perrun"] = false,
+    ["color_objective"] = "FFFFFFFF",
+    ["scale"] = 0.9,
+    ["color_prideful_value_warning"] = "FFFFFF00",
+    ["color_prideful_value_alert"] = "FFFF0000",
+    ["show_pull_values"] = false,
+    ["show_percent_numbers"] = true,
+    ["color_prideful_value"] = "FFFFFFFF",
+    ["color_current_pull"] = "FF00FF00",
+    ["color_chest_timeleft"] = "FF00FF00",
+    ["color_timeleft_expired"] = "FFFF0000",
+    ["color_chest_time_expired"] = "FF808080",
+    ["insert_keystone"] = true,
+    ["best_runs"] = {
+      [-1] = {
+        ["l30affixes-9-7-13-130"] = {
+          ["times"] = {
+            100, -- [1]
+          },
+        },
+      },
+    },
+    ["show_affixes_as_text"] = true,
+    ["color_deathcounter"] = "FFFFFFFF",
+    ["objective_time_delta_only"] = false,
+    ["color_dungeon_name"] = "FFFFD100",
+    ["hide_default_objectivetracker"] = true,
+    ["color_objective_completed"] = "FF808080",
+    ["objective_time"] = true,
+    ["npc_progress_id"] = "sl_1",
+    ["best_times"] = {
+      [-1] = {
+        50, -- [1]
+        ["l30affixes-9-7-13-130"] = {
+          123, -- [1]
+        },
+        ["l30"] = {
+          110, -- [1]
+        },
+      },
+    },
+    ["color_timeleft"] = "FF00FF00",
+    ["color_prideful"] = "FFFFFFFF",
+    ["npc_progress"] = {
+    },
+    ["color_time"] = "FFFFFFFF",
+  },
+}
